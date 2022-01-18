441ac22d08fa4be6e0b5a27079b10e60bf00f086
Daniel <d.t.nguyen@live.com>
2022-01-18 18:45:10 -0500

    moving API request and state management to Main component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import nasa_api from './nasa_api';
+import { Fragment } from 'react';
+import Header from './Header';
+import Main from './Main';
 
 function Application() {
-  const [data, setData] = useState("");
 
-  useEffect(() =>{
-		Promise.all([
-			nasa_api.get(`planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
-		]).then(res => setData(res[0].data));
-	},[]);
-  return <img src={data.url}></img>; 
+  return (
+    <Fragment>
+      <Header/>
+      <Main/>
+    </Fragment>
+  ) 
 }
 
 export default Application;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useState, useEffect} from 'react';
+import styled from 'styled-components';
+import nasa_api from './nasa_api';
 
 function Main() {
-  return <div></div>;
+
+  const [data, setData] = useState("");
+
+  useEffect(() =>{
+		Promise.all([
+			nasa_api.get(`planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
+		]).then(res => setData(res[0].data));
+	},[]);
+
+  return (
+    <Wrapper>
+      <Container>
+        <img src={data.url}></img>; 
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default Main;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  margin-top: 15px;
+`
+const Container = styled.div`
+  display: flex;
+  width = 80%;
+`
