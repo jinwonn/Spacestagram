afed9e764abb704d256b39dcac4e1abcb717de22
Daniel <d.t.nguyen@live.com>
2022-01-19 18:54:24 -0500

    fix error for deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import RocketIcon from '@mui/icons-material/Rocket';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
 import { IconButton } from '@mui/material';
 
 function Header() {
