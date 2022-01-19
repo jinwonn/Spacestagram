5fc61c5b7be68ec3489d1e6de18b608e3f9adc2e
Daniel <d.t.nguyen@live.com>
2022-01-18 22:25:33 -0500

    requesting 12 most recent daily photos upon first load

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,23 +2,41 @@ import { useState, useEffect} from 'react';
 import styled from 'styled-components';
 import nasa_api from './nasa_api';
 import Post from './Post';
+import moment from 'moment';
 
 function Main() {
 
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
+
+  const today = moment().format('YYYY-MM-DD');
+  
+  const twelveDaysAgo = (date) => {
+    return moment(date).subtract(12, 'days').format('YYYY-MM-DD');
+  };
+  
+  const newDay = twelveDaysAgo(today);
+  
 
   useEffect(() =>{
 		Promise.all([
-			nasa_api.get(`planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
+      nasa_api.get(`planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&start_date=${newDay}`)
 		]).then(res => {
       setData(res[0].data);
     }) 
 	},[]);
 
+  const parsedPosts = data.map((info)=> {
+      return (
+        <Post data={info}/>
+      )
+    }
+  );
+
+  console.log(parsedPosts);
   return (
     <Wrapper>
       <Container>
-        <Post data={data}/> 
+        {parsedPosts}
       </Container>
     </Wrapper>
   );
